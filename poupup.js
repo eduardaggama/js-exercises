ab1eb55614e52526466701dc81eeaa6f3c7294fe
Eduarda Gama <eduarda.gama@salg.com.br>
2018-09-02 13:12:03 -0300

    Update poupup.js

--- poupup.js
+++ poupup.js
@@ -1,12 +1,3 @@
-<html>
-  <head>
-    <title>exercise</title>
-  </head>
-  <body>
-    <h1>oi</h1>
-  <script>
     alert("oi");
     var ano = 2018;
     document.write("eu tenho" + (ano - 19));
-  </script>
-</html>
