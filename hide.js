2f903675c8ead7589e684fd19358a1a3849e5f57
Eduarda Gama <eduarda.gama@salg.com.br>
2018-09-24 11:51:16 -0300

    Create hide.js

---
+++ hide.js
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<p id="demo">olha só :)</p>
+
+<button type="button" onclick="document.getElementById('demo').style.display='none'">clique aqui</button>
+
+
+</body>
+</html>
